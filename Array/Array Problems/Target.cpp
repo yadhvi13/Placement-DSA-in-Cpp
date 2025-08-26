ba4834fb49ab6bfb8595cf57c44334b46f32cdfc
Yogeeta <yadhvi.ch@gmail.com>
2025-08-26 19:49:20 +0530

    practice questions

--- Array/Array Problems/Target.cpp
+++
@@ -1,7 +0,0 @@
-#include<iostream>
-using namespace std;
-
-int main(){
-    int arr[] = {2,3,11,13,15};
-    
-}
--- Array/Array Problems/tempCodeRunnerFile.cpp
+++
@@ -1,3 +0,0 @@
- // for(int i=0; i< size; i++){
-    //     cin >> arr[i];
-    // }
