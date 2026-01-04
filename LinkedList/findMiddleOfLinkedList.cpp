77ace247fb8baf73120a1fb95b3f3e98efa895b5
yadhvi.ch@gmail.com <yadhvi.ch@gmail.com>
2026-01-04 22:06:42 +0530

    main

---
+++ LinkedList/findMiddleOfLinkedList.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* next;
+    Node(int val) {
+        data = val;
+        next = NULL;
+    }
+};
+
+Node* middle(Node* head) {
+    Node* slow = head;
+    Node* fast = head;
+
+    while (fast && fast->next) {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    return slow;
+}
+
+int main() {
+    Node* head = new Node(1);
+    head->next = new Node(2);
+    head->next->next = new Node(3);
+    head->next->next->next = new Node(4);
+    head->next->next->next->next = new Node(5);
+
+    cout << middle(head)->data;
+    return 0;
+}
