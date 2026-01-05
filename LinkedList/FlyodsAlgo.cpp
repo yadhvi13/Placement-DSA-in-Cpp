1b28d04d46c076b2c2d9a18685dee2c279aa2ffe
yadhvi.ch@gmail.com <yadhvi.ch@gmail.com>
2026-01-05 22:07:19 +0530

    main

---
+++ LinkedList/FlyodsAlgo.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* next;
+    Node(int val) {
+        data = val;
+        next = NULL;
+    }
+};
+
+bool hasCycle(Node* head) {
+    Node* slow = head;
+    Node* fast = head;
+
+    while (fast && fast->next) {
+        slow = slow->next;
+        fast = fast->next->next;
+        if (slow == fast)
+            return true;
+    }
+    return false;
+}
+
+int main() {
+    Node* head = new Node(1);
+    head->next = new Node(2);
+    head->next->next = new Node(3);
+    head->next->next->next = head; // cycle
+
+    cout << hasCycle(head);
+    return 0;
+}
