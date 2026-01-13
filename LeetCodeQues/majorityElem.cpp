3b2ceb02ff62a96aca33d3fde7fb55a5d441f0c0
yadhvi13 <yadhvi.ch@gmail.com>
2026-01-13 18:37:27 +0530

    main

--- LeetCodeQues/majorityElem.cpp
+++ LeetCodeQues/majorityElem.cpp
@@ -31,41 +31,67 @@ using namespace std;
 // ----- using sorted array
 
 
-int majorityElem(vector<int>& nums){
-    int n = nums.size();
+// int majorityElem(vector<int>& nums){
+//     int n = nums.size();
 
-    // sort array
-    sort(nums.begin(), nums.end());
+//     // sort array
+//     sort(nums.begin(), nums.end());
 
-    // frequency count
-    int freq = 1 , ans = nums[0];
-    for(int i=1; i<n; i++){
-        if(nums[i] == nums[i-1]){
+//     // frequency count
+//     int freq = 1 , ans = nums[0];
+//     for(int i=1; i<n; i++){
+//         if(nums[i] == nums[i-1]){
+//             freq++;
+//         }
+//         else{
+//             freq = 1;
+//             ans = nums[i];
+//         }
+
+//         if(freq > n/2){
+//             return ans;
+//         }
+//     }
+//     return ans;  // majority element always exists
+// }
+
+// int main(){
+//     int n;
+//     cout << "enter the value of n: " ;
+//     cin >> n;
+
+//     vector<int> nums(n);
+//     cout << "enter the elements: ";
+//     for(int i=0; i<n;i++){
+//         cin >> nums[i];
+//     }
+
+//     cout << "majority element: " << majorityElem(nums) << endl;
+// return 0;
+// }
+
+
+
+
+// ------------ MOORE's Voting AGLORITHM ------
+//  -- count freq of an elem and take ans, ifd same elem milta hai toh freq ko ++ krdo, if different elem ho toh freq ko -- krdo
+int main(){
+    int nums[5] = {1,2,2,1,1};
+    int n;
+
+    int freq = 0, ans = 0;
+    for(int i=0; i<n; i++){
+        if(freq == 0){
+            ans = nums[i];
+            freq = 1;
+        }
+        else if(ans == nums[i]){
             freq++;
         }
         else{
-            freq = 1;
-            ans = nums[i];
-        }
-
-        if(freq > n/2){
-            return ans;
+            freq--;
         }
     }
-    return ans;  // majority element always exists
-}
-
-int main(){
-    int n;
-    cout << "enter the value of n: " ;
-    cin >> n;
-
-    vector<int> nums(n);
-    cout << "enter the elements: ";
-    for(int i=0; i<n;i++){
-        cin >> nums[i];
-    }
-
-    cout << "majority element: " << majorityElem(nums) << endl;
+    cout << "majority element : " << ans << endl;
 return 0;
 }
