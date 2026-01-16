d75cc2589a111c5b03572721a71419b4f95239a8
yadhvi13 <yadhvi.ch@gmail.com>
2026-01-16 21:24:04 +0530

    main

--- LeetCodeQues/productOfArrayExpectItself.cpp
+++ LeetCodeQues/productOfArrayExpectItself.cpp
@@ -49,3 +49,27 @@ int main(){
 return 0;
 }
 
+// ------- OPTIMIZED APPROACH ------- O(n) time and O(1) space
+class Solution {
+public:
+vector<int> productExceptSelf(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans(n,1);
+        // vector<int> prefix(n,1);
+        // vector<int> suffix(n,1);
+
+        // prefix in ans
+        for(int i=1; i<n; i++){
+            ans[i] = ans[i-1]*nums[i-1];
+        }
+
+        // suffix -- reverse loop
+        int suffix = 1;
+        for(int i=n-2; i>=0; i--){
+            suffix = suffix * nums[i+1];
+            ans[i] = ans[i]*suffix;
+        }
+
+        return ans;
+    }
+};
