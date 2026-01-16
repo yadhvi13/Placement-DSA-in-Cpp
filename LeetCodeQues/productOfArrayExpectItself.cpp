6cd252361e6d8c2c78d2395c164215b03a2a5166
yadhvi13 <yadhvi.ch@gmail.com>
2026-01-16 21:23:48 +0530

    main

--- LeetCodeQues/productOfArrayExpectItself.cpp
+++ LeetCodeQues/productOfArrayExpectItself.cpp
@@ -23,3 +23,29 @@ return 0;
 }
 
 
+// ------- OPTIMIZED APPROACH ------- O(n) time and O(n) space
+int main(){
+    int n;
+    int arr[4] = {1,2,3,4};
+    vector<int> prefix;
+    // left and right product of each element
+    prefix[0] = 1;
+    for(int i=1; i<n; i++){
+        prefix[i] = prefix[i-1]*arr[i-1];
+    }
+
+    // calculate suffix array for elem
+    vector<int> suffix;
+    // suffix[n-1] = 1;
+    for(int i=n-2; i>=0; i--){
+        suffix[i] = suffix[i+1] * arr[i+1];
+    }
+
+    // calculate ans array
+    vector<int> ans;
+    for(int i=0; i<n; i++){
+        ans[i] = prefix[i] * suffix[i];
+    }
+return 0;
+}
+
