348cdf8c4ef5f33fc70ec868c82cb2e9fe40b516
yadhvi13 <yadhvi.ch@gmail.com>
2026-02-04 19:59:47 +0530

    main

---
+++ LinkedList/CheckPalindromeLinkedList.cpp
@@ -0,0 +1,103 @@
+#include <iostream>
+using namespace std;
+
+// Node definition
+class Node {
+public:
+    int data;
+    Node* next;
+
+    Node(int val) {
+        data = val;
+        next = NULL;
+    }
+};
+
+// Insert at tail
+void insertAtTail(Node*& head, int val) {
+    Node* newNode = new Node(val);
+
+    if (head == NULL) {
+        head = newNode;
+        return;
+    }
+
+    Node* temp = head;
+    while (temp->next != NULL) {
+        temp = temp->next;
+    }
+    temp->next = newNode;
+}
+
+// Reverse linked list
+Node* reverseList(Node* head) {
+    Node* prev = NULL;
+    Node* curr = head;
+
+    while (curr != NULL) {
+        Node* nextNode = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = nextNode;
+    }
+    return prev;
+}
+
+// Check palindrome
+bool isPalindrome(Node* head) {
+    if (head == NULL || head->next == NULL)
+        return true;
+
+    // Step 1: Find middle
+    Node* slow = head;
+    Node* fast = head;
+
+    while (fast->next != NULL && fast->next->next != NULL) {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+
+    // Step 2: Reverse second half
+    Node* secondHalf = reverseList(slow->next);
+    Node* firstHalf = head;
+
+    // Step 3: Compare both halves
+    Node* temp = secondHalf;
+    while (temp != NULL) {
+        if (firstHalf->data != temp->data)
+            return false;
+        firstHalf = firstHalf->next;
+        temp = temp->next;
+    }
+
+    return true;
+}
+
+// Print linked list
+void printList(Node* head) {
+    while (head != NULL) {
+        cout << head->data << " -> ";
+        head = head->next;
+    }
+    cout << "NULL\n";
+}
+
+int main() {
+    Node* head = NULL;
+
+    // Example: 1 -> 2 -> 3 -> 2 -> 1
+    insertAtTail(head, 1);
+    insertAtTail(head, 2);
+    insertAtTail(head, 3);
+    insertAtTail(head, 2);
+    insertAtTail(head, 4);
+
+    printList(head);
+
+    if (isPalindrome(head))
+        cout << "Linked List is Palindrome \n";
+    else
+        cout << "Linked List is NOT Palindrome \n";
+
+    return 0;
+}
