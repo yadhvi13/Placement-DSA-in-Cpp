e785b30ffcf5b572aafe43d32310f222fc10ee67
yadhvi13 <yadhvi.ch@gmail.com>
2026-02-12 21:35:45 +0530

    main

---
+++ LeetCodeQues/ReverseaLinkedList.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+using namespace std;
+
+// Definition of ListNode
+class ListNode {
+public:
+    int val;
+    ListNode* next;
+
+    ListNode(int x) {
+        val = x;
+        next = NULL;
+    }
+};
+
+// Function to reverse linked list
+ListNode* reverseList(ListNode* head) {
+
+    ListNode* prev = NULL;
+    ListNode* curr = head;
+
+    while (curr != NULL) {
+
+        ListNode* nextNode = curr->next; // Store next node
+        curr->next = prev;               // Reverse link
+        prev = curr;                     // Move prev
+        curr = nextNode;                 // Move curr
+    }
+
+    return prev;  // New head
+}
+
+// Function to print list
+void printList(ListNode* head) {
+    while (head != NULL) {
+        cout << head->val << " ";
+        head = head->next;
+    }
+    cout << endl;
+}
+
+int main() {
+
+    // Creating linked list: 1 -> 2 -> 3 -> 4 -> 5
+    ListNode* head = new ListNode(1);
+    head->next = new ListNode(2);
+    head->next->next = new ListNode(3);
+    head->next->next->next = new ListNode(4);
+    head->next->next->next->next = new ListNode(5);
+
+    cout << "Original List: ";
+    printList(head);
+
+    head = reverseList(head);
+
+    cout << "Reversed List: ";
+    printList(head);
+
+    return 0;
+}
