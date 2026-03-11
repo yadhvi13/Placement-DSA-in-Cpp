535f3f06edf73daeb69cba8c94a5bdbceec1cdec
yadhvi13 <yadhvi.ch@gmail.com>
2026-03-11 20:26:13 +0530

    main

---
+++ LeetCodeQues/PreviousPermutation.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+void previousPermutation(vector<int>& nums) {
+    int n = nums.size();
+    int i = n - 2;
+
+    while(i >= 0 && nums[i] <= nums[i + 1]) {
+        i--;
+    }
+
+    if(i >= 0) {
+        int j = n - 1;
+        while(nums[j] >= nums[i]) {
+            j--;
+        }
+        swap(nums[i], nums[j]);
+    }
+
+    reverse(nums.begin() + i + 1, nums.end());
+}
+
+int main() {
+
+    vector<int> nums = {1,3,2};
+
+    previousPermutation(nums);
+
+    for(int x : nums)
+        cout << x << " ";
+
+    return 0;
+}
