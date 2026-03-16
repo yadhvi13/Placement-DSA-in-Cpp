be6b585e340836475f5e04ed754b0c619732b808
yadhvi13 <yadhvi.ch@gmail.com>
2026-03-16 21:15:09 +0530

    main

---
+++ LeetCodeQues/LowestCommonAncestor.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+
+    TreeNode(int x) {
+        val = x;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q){
+
+    if(root == NULL || root == p || root == q)
+        return root;
+
+    TreeNode* left = lowestCommonAncestor(root->left, p, q);
+    TreeNode* right = lowestCommonAncestor(root->right, p, q);
+
+    if(left != NULL && right != NULL)
+        return root;
+
+    if(left != NULL)
+        return left;
+
+    return right;
+}
+
+int main(){
+
+    TreeNode* root = new TreeNode(9);
+    root->left = new TreeNode(5);
+    root->right = new TreeNode(1);
+    root->left->left = new TreeNode(6);
+    root->left->right = new TreeNode(2);
+    root->right->left = new TreeNode(0);
+    root->right->right = new TreeNode(8);
+
+    TreeNode* p = root->left;
+    TreeNode* q = root->right;
+
+    TreeNode* ans = lowestCommonAncestor(root,p,q);
+
+    cout<<"LCA: "<<ans->val;
+
+    return 0;
+}
