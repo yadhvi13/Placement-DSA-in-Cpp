776afb42c37f11c14ad7c6c8a955103e6fc47fab
yadhvi13 <yadhvi.ch@gmail.com>
2026-04-09 20:28:49 +0530

    main

---
+++ LeetCodeQues/DeleteNodeInLL.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+
+class ListNode {
+public:
+    int val;
+    ListNode* next;
+
+    ListNode(int x) {
+        val = x;
+        next = NULL;
+    }
+};
+
+// Function to delete node
+void deleteNode(ListNode* node) {
+    node->val = node->next->val;
+    ListNode* temp = node->next;
+    node->next = node->next->next;
+    delete temp;
+}
+
+// Helper function to print list
+void printList(ListNode* head) {
+    while (head != NULL) {
+        cout << head->val << " -> ";
+        head = head->next;
+    }
+    cout << "NULL" << endl;
+}
+
+int main() {
+    // Create linked list: 4 -> 5 -> 1 -> 9
+    ListNode* head = new ListNode(4);
+    head->next = new ListNode(5);
+    head->next->next = new ListNode(1);
+    head->next->next->next = new ListNode(9);
+
+    cout << "Original List: ";
+    printList(head);
+
+    // Delete node with value 5
+    deleteNode(head->next);
+
+    cout << "After Deletion: ";
+    printList(head);
+
+    return 0;
+}
