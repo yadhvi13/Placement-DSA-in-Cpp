fb1262dd29d205cbd2882786179ad2947af71162
yadhvi13 <yadhvi.ch@gmail.com>
2026-06-19 20:40:52 +0530

    main

---
+++ Strivers-Sheet/RearrangeElementBySign.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> rearrangeArray(vector<int>& nums) {
+    int n = nums.size();
+    vector<int> ans(n);
+
+    int pos = 0;
+    int neg = 1;
+
+    for (int i = 0; i < n; i++) {
+        if (nums[i] > 0) {
+            ans[pos] = nums[i];
+            pos += 2;
+        } else {
+            ans[neg] = nums[i];
+            neg += 2;
+        }
+    }
+
+    return ans;
+}
+
+int main() {
+    vector<int> nums = {3, 1, -2, -5, 2, -4};
+
+    vector<int> result = rearrangeArray(nums);
+
+    for (int x : result) {
+        cout << x << " ";
+    }
+
+    return 0;
+}
