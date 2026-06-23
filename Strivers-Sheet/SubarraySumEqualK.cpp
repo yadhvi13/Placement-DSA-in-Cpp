1422f56b83224b7c5d2b24495d8077c39da4529f
yadhvi13 <yadhvich@gmail.com>
2026-06-23 19:18:00 +0530

    main

---
+++ Strivers-Sheet/SubarraySumEqualK.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+int subarraySum(vector<int>& nums, int k) {
+    unordered_map<int, int> mp;
+    mp[0] = 1;
+
+    int prefixSum = 0;
+    int count = 0;
+
+    for (int num : nums) {
+        prefixSum += num;
+
+        if (mp.find(prefixSum - k) != mp.end()) {
+            count += mp[prefixSum - k];
+        }
+
+        mp[prefixSum]++;
+    }
+
+    return count;
+}
+
+int main() {
+    vector<int> nums = {1, 1, 1};
+    int k = 2;
+
+    cout << subarraySum(nums, k) << endl;
+
+    return 0;
+}
