c8411fe30c4e3402e070c0db957c04509ef71341
yadhvi13 <yadhvi.ch@gmail.com>
2026-07-22 20:05:52 +0530

    main

---
+++ Strivers-Sheet/SetMatrixZero.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    int m, n;
+
+    cout << "Enter rows and columns: ";
+    cin >> m >> n;
+
+    vector<vector<int>> matrix(m, vector<int>(n));
+
+    cout << "Enter matrix:\n";
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cin >> matrix[i][j];
+        }
+    }
+
+    vector<int> row(m, 0);
+    vector<int> col(n, 0);
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (matrix[i][j] == 0)
+            {
+                row[i] = 1;
+                col[j] = 1;
+            }
+        }
+    }
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (row[i] == 1 || col[j] == 1)
+            {
+                matrix[i][j] = 0;
+            }
+        }
+    }
+
+    cout << "\nMatrix after setting zeroes:\n";
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
