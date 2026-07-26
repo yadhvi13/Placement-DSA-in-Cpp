7d512864ee262b3f235bf8f494e3ace592a4d317
yadhvi13 <yadhvi.ch@gmail.com>
2026-07-26 19:25:10 +0530

    main

---
+++ Strivers-Sheet/FibonacciNumber.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int fib(int n) {
+    if (n == 0)
+        return 0;
+    if (n == 1)
+        return 1;
+
+    int prev2 = 0;
+    int prev1 = 1;
+
+    for (int i = 2; i <= n; i++) {
+        int curr = prev1 + prev2;
+        prev2 = prev1;
+        prev1 = curr;
+    }
+
+    return prev1;
+}
+
+int main() {
+    int n;
+
+    cout << "Enter n: ";
+    cin >> n;
+
+    cout << "Fibonacci Number: " << fib(n) << endl;
+
+    return 0;
+}
