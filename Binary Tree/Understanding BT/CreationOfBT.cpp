ea66ac0e595aa799d536eb0ebf9abaccc96102b7
yadhvi13 <yadhvi.ch@gmail.com>
2026-07-31 20:42:59 +0530

    main

---
+++ Binary Tree/Understanding BT/CreationOfBT.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+using namespace std;
+
+class node{
+    public:
+      int data;
+      node * left;
+      node * right;
+
+    //   constructor for new node 
+       node(int d){
+        this->data = d;
+        this->left = NULL;
+        this->right = NULL;
+       }
+};
+
+// function for create tree
+node* buildTree(node* root){
+   cout << "Enter the data: " << endl;
+   int data;
+   cin >> data;
+   root = new node(data);
+
+   if(data == -1){
+    return NULL;
+   }
+
+    cout << "Enter data for inserting in left of " << data << endl;
+    root->left = buildTree(root->left);
+
+    cout << "Enter data for inserting in right of " << data << endl;
+    root->right = buildTree(root->right);
+
+    return root;
+}
+
+int main(){
+
+    node* root = NULL;
+
+    // creating a Tree
+    root = buildTree(root);
+
+    return 0;
+}
